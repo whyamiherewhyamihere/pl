8032d0b2d363abaab4aa71068ffb612b30ed9e95
whyamiherewhyamihere <119357669+whyamiherewhyamihere@users.noreply.github.com>
2023-12-24 00:22:15 +0300

    Create TaskItem.cs

---
+++ lab4/Model/TaskItem.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace laba4.Model;
+
+public class TaskItem
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set;}
+    public DateTime Deadline { get; set;}
+    public string Tags { get; set;}
+
+    public TaskItem(string title, string description, DateTime deadline, string tags)
+    {
+        Title = title;
+        Description = description;
+        Deadline = deadline;
+        Tags = tags;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title}\nDescription: {Description}\nDeadline: {Deadline:dd.MM.yyyy}\nTags: {Tags}";
+    }
+}
